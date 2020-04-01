d91b37963cf9693a5b95b5a205b8d4c4d5983840
Aider <ideeer4@gmail.com>
2020-04-02 01:27:15 +0300

    Add bioinf constants

---
+++ src/constants/orf.ts
@@ -0,0 +1,10 @@
+// Any ORF start coddong
+export const START_CODDON = "AUG";
+// RNA stop codons
+export const STOP_CODDON_RNA1 = "UAA";
+export const STOP_CODDON_RNA2 = "UAG";
+export const STOP_CODDON_RNA3 = "UGA";
+// DNA stop codon
+export const STOP_CODDON_DNA1 = "TAA";
+export const STOP_CODDON_DNA2 = "TAG";
+export const STOP_CODDON_DNA3 = "TGA";
