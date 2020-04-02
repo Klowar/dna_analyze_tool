4fd16a08f320dc9fe671e446331e36831d9704ab
Aider <ideeer4@gmail.com>
2020-04-02 14:07:26 +0300

    Window managing stuf

--- src/utils/window_util.ts
+++ src/utils/window_util.ts
@@ -4,7 +4,7 @@ import { createWindow } from './../view/welcome/index';
 // const windows = new Map<string, BrowserWindow>();
 let currWindow: BrowserWindow = null;
 
-const displayStack = ["welcome"];
+const displayStack: number[] = [];
 
 export const handleRouting = async (name?: string): Promise<void> => {
     if (name === undefined) {
@@ -15,11 +15,26 @@ export const handleRouting = async (name?: string): Promise<void> => {
     if (module === undefined) {
         return;
     }
-    currWindow = module.default();
-    displayStack.push(name);
+    const temp: BrowserWindow = module.default();
+    currWindow.hide();
+    currWindow = temp;
+    temp.show();
+    displayStack.push(currWindow.id);
 }
 
 export const firstInit = (): void => {
     currWindow = createWindow();
+    displayStack.push(currWindow.id);
     handleRouting();
+}
+
+export const openPrevWindow = (): void => {
+    displayStack.length -= 1;
+    const windows = BrowserWindow.getAllWindows();
+    const win = windows.find(_ => _.id === displayStack[displayStack.length - 1]);
+    if (win) win.show();
+}
+
+export const closeAllWindows = (): void => {
+    BrowserWindow.getAllWindows().forEach(_ => { if (_.id !== currWindow.id) _.destroy() });
 }
