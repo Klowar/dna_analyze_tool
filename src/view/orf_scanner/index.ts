e92b7db6b3e2c53cd2da9af144f8a371460ca910
Aider <ideeer4@gmail.com>
2020-04-03 23:50:30 +0300

    Fix issues with build

--- src/view/orf_scanner/index.ts
+++ src/view/orf_scanner/index.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 export const loadScannerWindow = (win: BrowserWindow): BrowserWindow => {
     // Create the browser window.
 
-    win.loadFile(path.join(__dirname, './scanner.html'));
+    win.loadFile(path.join(__dirname, 'scanner.html'));
     // window.webContents.openDevTools();
     return win;
 };
--- src/view/welcome/index.ts
+++ src/view/welcome/index.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 export const loadWelcomeWindow = (win: BrowserWindow): BrowserWindow => {
     // Create the browser window.
 
-    win.loadFile(path.join(__dirname, './welcome.html'));
+    win.loadFile(path.join(__dirname, 'welcome.html'));
 
     return win;
 };
