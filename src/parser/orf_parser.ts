80719dc363ed7277cd0083fbcbc718e7a50be9e7
Aider <ideeer4@gmail.com>
2020-04-03 19:05:21 +0300

    Update parser RegExp's to search orfs

--- src/parser/orf_parser.ts
+++ src/parser/orf_parser.ts
@@ -1,22 +1,47 @@
-import { START_CODDON, STOP_CODDON_DNA1, STOP_CODDON_DNA2, STOP_CODDON_DNA3, STOP_CODDON_RNA1, STOP_CODDON_RNA2, STOP_CODDON_RNA3 } from './../constants/orf';
+import { START_CODDON_DNA, STOP_CODDON_DNA1, STOP_CODDON_DNA2, STOP_CODDON_DNA3, STOP_CODDON_RNA1, STOP_CODDON_RNA2, STOP_CODDON_RNA3 } from './../constants/orf';
+
+
+const findSmallest = (str: string, prefix = START_CODDON_DNA): string[] => {
+    const arr: string[] = [];
+
+    const codons = str.match(/.{1,3}/g);
+    codons.forEach((elem, index) => {
+        if (elem === prefix && index !== 0) {
+            arr.push(
+                codons.slice(index).join('')
+            )
+        }
+    })
+    return arr;
+}
+
+const findSmallestForArray = (arr: RegExpMatchArray | string[]): string[] => {
+    const result: string[][] = [arr];
+
+    for (const str of arr) {
+        if (str === null) continue;
+        result.push(findSmallest(str));
+    }
+    return result.flat(1);
+}
 
 export const orfParser = (str: string): string[] => {
 
     const regs = [
-        new RegExp(`${START_CODDON}.+${STOP_CODDON_DNA1}`, "g"),
-        new RegExp(`${START_CODDON}.+${STOP_CODDON_DNA2}`, "g"),
-        new RegExp(`${START_CODDON}.+${STOP_CODDON_DNA3}`, "g"),
-        new RegExp(`${START_CODDON}.+${STOP_CODDON_RNA1}`, "g"),
-        new RegExp(`${START_CODDON}.+${STOP_CODDON_RNA2}`, "g"),
-        new RegExp(`${START_CODDON}.+${STOP_CODDON_RNA3}`, "g"),
+        new RegExp(`${START_CODDON_DNA}(\\w{3})+?${STOP_CODDON_DNA1}`, "ig"),
+        new RegExp(`${START_CODDON_DNA}(\\w{3})+?${STOP_CODDON_DNA2}`, "ig"),
+        new RegExp(`${START_CODDON_DNA}(\\w{3})+?${STOP_CODDON_DNA3}`, "ig"),
     ];
 
     const orfs: string[][] = [];
-
     for (const reg of regs) {
-        orfs.push(
-            str.match(reg)
-        )
+        const matches = str.matchAll(reg);
+        for (const temp of matches) {
+            orfs.push(
+                findSmallestForArray(temp)
+            );
+        }
     }
+
     return orfs.flat(Infinity);
 }
