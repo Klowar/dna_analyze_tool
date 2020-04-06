dff8ef7325c999663df33bc4b1cef5d441cf8a19
Aider <ideeer4@gmail.com>
2020-04-06 14:55:27 +0300

    Public page using react

--- public/welcome/welcome.tsx
+++ public/welcome/welcome.tsx
@@ -1,20 +1,8 @@
-import React from 'react'; // eslint-disable-line 
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './components/App';
 
 ReactDOM.render(
-    <div>Lol</div>,
+    <App />,
     document.getElementById('root')
 );
-// include the ipc module to communicate with main process.
-// const ipcRenderer = require('electron').ipcRenderer;
-
-// const a = document.getElementsByTagName('a');
-// for (const _ of a) {
-//     _.addEventListener('click', function (e) {
-//         //send the info to main process . we can pass any arguments as second param.
-//         console.log(e.target.id);
-
-//         ipcRenderer.send("route", e.target.id);
-//     });
-// }
-
