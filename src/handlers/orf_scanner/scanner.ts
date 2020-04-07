e4cd3535677d6869bb247d896006214160662df3
Aider <ideeer4@gmail.com>
2020-04-07 22:22:13 +0300

    Refactor for code sharing

--- src/handlers/orf_scanner/scanner.ts
+++ src/handlers/orf_scanner/scanner.ts
@@ -1,12 +1,21 @@
+import { CODDON_LENGTH } from './../../constants/orf';
 import { orfParser } from './../../parser/orf_parser';
 import { allRules } from './../../rule/orf_rules';
 import { HandlerType } from './../types';
 
+const dynamicLengRule = (orf: string, length: number): boolean => {
+    return orf.length >= length;
+}
 
 export const handleOrfSearch: HandlerType<string[]> = (event, ...args): Promise<string[]> => {
     return new Promise((res) => {
         const potentialOrf = orfParser(args[0]);
-        const orfs = potentialOrf.filter(_ => _ && allRules(_));
+
+        const orfs = potentialOrf.filter(
+            _ => _
+                && allRules(_)
+                && dynamicLengRule(_, args[1] || CODDON_LENGTH)
+        );
 
         res(orfs);
     })
